703d94faf15c9692959242a39c24a40897dadc0b
Roel Magdaleno <roelmagdaleno@gmail.com>
2024-06-08 15:46:50 -0600

    Add like and unlike setting

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'cc76d89612806e480242');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'd4cd4d3d8c65b6c5f5c2');
